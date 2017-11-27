851e3f5125bfb626659e59a004b68dd0909da987
iKeansMac <imkean@163.com>
2017-11-27 17:13:43 +0800

    added powerline fonts

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 cp ~/.vimrc ~/.vimrc.bkp
 cp -f ./vimrc.vim ~/.vimrc
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --depth=1 https://github.com/powerline/fonts.git  ~/.vim/fonts &&
+    ~/.vim/fonts/install.sh
+git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim  "+set nomore" "+BundleInstall!"  "+BundleClean" "+qall"
