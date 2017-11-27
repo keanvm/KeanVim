f1ac2798c722cc6f7becc637b7b4fbb800d02861
iKeansMac <imkean@163.com>
2017-11-27 17:00:49 +0800

    enabled to auto BundleInstall

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-
+cp ~/.vimrc ~/.vimrc.bkp
 cp -f ./vimrc.vim ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim  "+set nomore" "+BundleInstall!"  "+BundleClean" "+qall"
