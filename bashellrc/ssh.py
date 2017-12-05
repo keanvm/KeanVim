0f4220b47f63d97aa4472f9039567dea8800eed0
iKeansMac <imkean@163.com>
2017-12-05 15:05:08 +0800

    updated bashrc install way

--- bashellrc/ssh.py
+++ bashellrc/ssh.py
@@ -31,10 +31,10 @@ def main():
     import sys
     argv = sys.argv
     from os.path import expanduser
-    file_name = "{}/.sshrc".format(expanduser("~"))
+    file_name = "{}/.bashellrc/sshrc".format(expanduser("~"))
     len_argv = len(argv)
     if len_argv < 2 or len_argv > 3:
-        print("usage:\n\t_ssh.py [name] <config_file>\n")
+        print("usage:\n\tssh.py <name> [config_file]\n")
         return False
     if len_argv >= 2:
         host_name = argv[1]
