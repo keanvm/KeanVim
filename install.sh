dfc8bb3a88f7d130b9257b95e4f596af5ad5fd9a
iKeansMac <imkean@163.com>
2018-10-19 19:31:28 +0800

    updated

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #! /bin/bash
 
 
-cp -rf bashellrc  ~/.bashellrc
-cp vimrc.vim ~/.bashellrc/vimrc.vim
-
-printf "
+[[ -d ~/.bashellrc ]] ||  printf "
 if [ -f ~/.bashellrc/bashrc ]; then
     . ~/.bashellrc/bashrc
 fi
 " >> ~/.bashrc
 
+cp -rf bashellrc  ~/.bashellrc
+cp vimrc.vim ~/.bashellrc/vimrc.vim
 
 
-git clone --depth=1 https://github.com/powerline/fonts.git  ~/.vim/fonts &&
-    ~/.vim/fonts/install.sh
-git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[[ -d ~/.vim/fonts ]] || git clone --depth=1 https://github.com/powerline/fonts.git  ~/.vim/fonts &&  ~/.vim/fonts/install.sh
+[[ -d ~/.vim/bundle/Vundle.vim ]] || git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim -u "~/.bashellrc/vimrc.vim"  "+set nomore" "+BundleInstall!"  "+BundleClean" "+qall"
